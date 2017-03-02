ed09c39b7ad124f44775af398dc674c76c33b13a
刘高升 <liugaosheng@liugaoshengdemacbook-pro.local>
2017-03-02 09:58:24 +0800

    Initial Commit

---
+++ liubaisiBeta/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  liubaisiBeta
+//
+//  Created by 刘高升 on 17/3/2.
+//  Copyright © 2017年 刘高升. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
